3ab0d340139ce27712925e3361c57e7a5ea2270f
achorostian <andrzejchorostian@outlook.com>
2019-04-02 22:26:35 +0200

    Add: NewUserTest

--- src/test/java/RestaurantRegisterTest/UserTest.java
+++ src/test/java/RestaurantRegisterTest/UserTest.java
@@ -1,16 +1,49 @@
 package RestaurantRegisterTest;
 import RestaurantRegister.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterEach;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class UserTest
+import java.util.List;
+
+class NewUserTest
 {
-    @Test
-    public void initTest()
+    private User user;
+
+    @BeforeEach
+    public void setUp()
     {
-        assertTrue(true);
+        user = new User("Jan Kowalski" , "jkowalski@gmail.com","123-543-678");
+    }
+
+    @Test
+    public void ClassTest()
+    {
+        assertThat(user).isInstanceOf(User.class);
+    }
+
+    @Test
+    public void FullNameTest()
+    {
+        assertThat(user.getFullName()).isEqualToIgnoringCase("jan kowalski");
+    }
+
+    @Test
+    public void eMailTest()
+    {
+        assertThat(user.getEMail()).isNotEmpty();
+    }
+
+    @Test
+    public void phoneTest()
+    {
+        assertThat(user).hasFieldOrPropertyWithValue("phone","123-543-678");
+    }
+
+    @Test
+    public void reservationListTest()
+    {
+        assertThat(user.getReservationsList()).isInstanceOf(List.class).hasSize(0);
     }
 }
