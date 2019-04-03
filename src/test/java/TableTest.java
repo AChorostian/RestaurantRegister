cf18868aae32ada73298309c69c882ac965280ed
achorostian <andrzejchorostian@outlook.com>
2019-04-03 23:54:38 +0200

    Polish: deleted unused imports

--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -3,13 +3,11 @@ import static org.hamcrest.Matchers.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import sun.tools.jconsole.Tab;
 
 import java.util.ArrayList;
 import java.util.Arrays;
