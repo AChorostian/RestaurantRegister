f5e7683de2fb7ae7eb1395b25c74aef1664d98c2
achorostian <andrzejchorostian@outlook.com>
2019-04-03 15:48:48 +0200

    Add: userIdTest

--- src/test/java/RestaurantRegisterTest/UserTest.java
+++ src/test/java/RestaurantRegisterTest/UserTest.java
@@ -2,9 +2,13 @@ package RestaurantRegisterTest;
 import RestaurantRegister.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class NewUserTest
@@ -18,13 +22,13 @@ class NewUserTest
     }
 
     @Test
-    public void ClassTest()
+    public void classTest()
     {
         assertThat(user).isInstanceOf(User.class);
     }
 
     @Test
-    public void FullNameTest()
+    public void fullNameTest()
     {
         assertThat(user.getFullName()).isEqualToIgnoringCase("jan kowalski");
     }
@@ -47,3 +51,64 @@ class NewUserTest
         assertThat(user.getReservationsList()).isInstanceOf(List.class).hasSize(0);
     }
 }
+
+class UserIdTest
+{
+    private static List<IUser> users;
+
+    @BeforeAll
+    public static void setUp()
+    {
+        users = new ArrayList<>();
+        users.add( new User("Jan Kowalski" , "jkowalski@gmail.com","123-543-678"));
+        users.add( new User("Jan Nowak" , "jnowak@gmail.com","432623342"));
+        users.add( new User("Tomasz Nowak" , "tnowak@gmail.com","+48386746251"));
+        users.add( new User("Katarzyna Nowak" , "knowak@gmail.com","+48 386 746 251"));
+    }
+
+    @Test
+    public void positiveNumberIdTest()
+    {
+        assertThat(users).allMatch(u -> u.getId() >= 0 );
+    }
+
+    @Test
+    public void growingIdTest()
+    {
+        assertThat(users).extracting(IUser::getId).containsOnly(0,1,2,3);
+    }
+
+    @Test
+    public void incrementOrderIdTest()
+    {
+        assertThat(users).extracting(IUser::getId).containsExactly(0,1,2,3);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
