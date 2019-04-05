3d5c458ce43add717a73f60b094076131282eaa3
achorostian <andrzejchorostian@outlook.com>
2019-04-05 15:33:53 +0200

    Add: ReservationIdTest class

--- src/test/java/ReservationTest.java
+++ src/test/java/ReservationTest.java
@@ -3,22 +3,14 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
-
-
-class ReservationTest
-{
-    @Test
-    public void initTest()
-    {
-        assertTrue(true);
-    }
-}
 
 class NewReservationTest
 {
@@ -69,14 +61,64 @@ class NewReservationTest
     }
 
     @Test
-    public void phoneTest()
+    public void userTest()
     {
         assertThat(reservation.getUser()).isNotNull();
     }
 
     @Test
-    public void reservationsTest()
+    public void tableTest()
     {
         assertThat(reservation.getTable()).isNotNull();
+    }
+}
+
+class ReservationIdTest
+{
+    private static List<IReservation> reservations;
+    private static int beforeIdCounter;
+
+    @BeforeAll
+    public static void setUp()
+    {
+        LocalTime rStartTime = LocalTime.of(10,0);
+        LocalTime rEndTime = LocalTime.of(20,0);
+        Restaurant restaurant = new Restaurant("Stacja","Gdańsk Al. Grunwaldzka 111",rStartTime,rEndTime);
+        User user = new User("Jan Kowalski" , "jkowalski@gmail.com","123-543-678",restaurant);
+        Table table = new Table("nr. 5",5,restaurant);
+
+        LocalTime startTime = LocalTime.of(11,0);
+        LocalTime endTime = LocalTime.of(11,30);
+        Reservation.resetIdCounter();
+        beforeIdCounter = Reservation.getIdCounter();
+        reservations = new ArrayList<>();
+        reservations.add(new Reservation(3,startTime,endTime, LocalDate.now().plusDays(5),user,table));
+        reservations.add(new Reservation(3,startTime.plusHours(1),endTime.plusHours(1), LocalDate.now().plusDays(5),user,table));
+        reservations.add(new Reservation(3,startTime.plusHours(2),endTime.plusHours(2), LocalDate.now().plusDays(5),user,table));
+        reservations.add(new Reservation(3,startTime.plusHours(3),endTime.plusHours(3), LocalDate.now().plusDays(5),user,table));
+    }
+
+    @Test
+    public void changingIdCounterTest()
+    {
+        assertThat(Reservation.getIdCounter()).isEqualTo(beforeIdCounter+4);
+    }
+
+    @Test
+    public void positiveNumberIdTest()
+    {
+        assertThat(reservations).allMatch(u -> u.getId() >= 0 );
+    }
+
+    @Test
+    public void increasingIdTest()
+    {
+        assertThat(reservations).extracting(IReservation::getId).containsOnly(0,1,2,3);
+    }
+
+    @Test
+    public void incrementOrderIdTest()
+    {
+        assertThat(reservations).extracting(IReservation::getId).containsExactly(0,1,2,3);
     }
 }
